6692d32010f121c3df8bc05b4106080f8ed44389
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:05:50 -0500

    1 file modified, 1 added

---
+++ src/app/service/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+    //url:'https://rulesly.intocables.net/31574849548976465468764002013123411'
+    url:'https://ruleslys.herokuapp.com/31574849548976465468764002013123411/'
+}
--- src/app/service/userpunish.service.ts
+++ src/app/service/userpunish.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { GLOBAL } from "./global";
 @Injectable()
 export class UserpunishService {
+  public url : string;
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) {
+    this.url = GLOBAL.url;
+   }
+   getUsers() : Observable<any>{
+    return this._http.get(this.url+'Gaaaaaa')
+  }
 
 }
