3b96105f68f396855af4a113b3dd7083b0d544db
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:01:45 -0500

    1 file modified, 2 added

---
+++ src/app/table-rulesly/table-rulesly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-rulesly',
+  templateUrl: './table-rulesly.component.html',
+  styleUrls: ['./table-rulesly.component.css']
+})
+export class TableRuleslyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
