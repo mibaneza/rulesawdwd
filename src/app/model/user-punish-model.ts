6d21122ad889657a072ed3870ec035d37de7144a
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:27:36 -0500

    1 file added

---
+++ src/app/model/user-punish-model.ts
@@ -0,0 +1,8 @@
+export class UserPunishModel {
+   constructor(
+        public  added : string,
+        public  types: string,
+        public  reason : string,
+        public  id: number
+         ){}
+        }
