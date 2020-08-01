56dadf49e00b30bbbc3b21fc7e3474fca9c61817
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:04:07 -0500

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TableRuleslyComponent } from './table-rulesly/table-rulesly.component';
+import { UserpunishService } from './service/userpunish.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, TableRuleslyComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [UserpunishService]
 })
 export class AppModule { }
