af2683e7ecbc6640f6c8d39d0a00c40e466531ef
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:08:58 -0500

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { NgxIndexedDBModule, DBConfig } from 'ngx-indexed-db';
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TableRuleslyComponent } from './table-rulesly/table-rulesly.component';
 import { UserpunishService } from './service/userpunish.service';
+import { RolplayComponent } from './rolplay/rolplay.component';
+
+
+const dbConfig: DBConfig  = {
+  name: 'ruleslyDB',
+  version: 1,
+  objectStoresMeta: [{
+    store: 'rolplay-ban',
+    storeConfig: { keyPath: 'id', autoIncrement: true },
+    storeSchema: [
+      { name: 'added', keypath: 'added', options: { unique: false } },
+      { name: 'type', keypath: 'type', options: { unique: false } },
+      { name: 'reason', keypath: 'reason', options: { unique: false } }
+    ]
+  }]
+};
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TableRuleslyComponent ],
+  declarations: [ 
+    AppComponent,
+    HelloComponent,
+    TableRuleslyComponent,
+    RolplayComponent,
+    HttpClientModule,
+    NgxIndexedDBModule.forRoot(dbConfig) ],
   bootstrap:    [ AppComponent ],
   providers: [UserpunishService]
 })
---
+++ src/app/rolplay/rolplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rolplay',
+  templateUrl: './rolplay.component.html',
+  styleUrls: ['./rolplay.component.css']
+})
+export class RolplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
