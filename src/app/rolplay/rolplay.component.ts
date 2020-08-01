e6a46cd7016f6afe3bccc4d022095f04aef9b4c4
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-01 13:21:20 -0500

    1 file modified

--- src/app/rolplay/rolplay.component.ts
+++ src/app/rolplay/rolplay.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxIndexedDBService } from 'ngx-indexed-db';
-
+import { IndexedDbService } from '../service/indexed-db.service';
 @Component({
   selector: 'app-rolplay',
   templateUrl: './rolplay.component.html',
